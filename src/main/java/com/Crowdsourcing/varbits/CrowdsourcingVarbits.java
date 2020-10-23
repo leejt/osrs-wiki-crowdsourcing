507935405aec8e02d162e8d6dd395d8153cf28eb
Andrew McAdams <andmcadams@gmail.com>
2020-10-23 18:28:17 -0500

    Avoid collecting varbits set on login

--- src/main/java/com/Crowdsourcing/varbits/CrowdsourcingVarbits.java
+++ src/main/java/com/Crowdsourcing/varbits/CrowdsourcingVarbits.java
@@ -4,7 +4,9 @@ import com.Crowdsourcing.CrowdsourcingManager;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import javax.inject.Inject;
+import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.Client;
+import net.runelite.api.GameState;
 import net.runelite.api.IndexDataBase;
 import net.runelite.api.VarbitComposition;
 import net.runelite.api.coords.WorldPoint;
@@ -13,6 +15,7 @@ import net.runelite.api.events.VarbitChanged;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.eventbus.Subscribe;
 
+@Slf4j
 public class CrowdsourcingVarbits
 {
 
@@ -27,11 +30,12 @@ public class CrowdsourcingVarbits
 
 	private static final int VARBITS_ARCHIVE_ID = 14;
 
-	private int tick = 0;
 	private int[] oldVarps = null;
 	private Multimap<Integer, Integer> varbits;
 
-	private void initialize()
+	private int initializingTick = 0;
+
+	public void startUp()
 	{
 		varbits = HashMultimap.create();
 
@@ -56,7 +60,7 @@ public class CrowdsourcingVarbits
 		});
 	}
 
-	private void teardown()
+	public void shutDown()
 	{
 		varbits = null;
 		oldVarps = null;
@@ -65,17 +69,20 @@ public class CrowdsourcingVarbits
 	@Subscribe
 	public void onGameStateChanged(GameStateChanged gameStateChanged)
 	{
-		// Due to the number of varbits loaded on login (approx 2k+), we want to avoid capturing
-		// the first few ticks on login.
-
+		if (gameStateChanged.equals(GameState.HOPPING) || gameStateChanged.equals(GameState.LOGGING_IN))
+		{
+			initializingTick = client.getTickCount();
+			shutDown();
+			startUp();
+		}
 	}
 
 	@Subscribe
 	public void onVarbitChanged(VarbitChanged varbitChanged)
 	{
-		// Whenever a varbit is changed, record it and pass the info off to be submitted.
 		int tick = client.getTickCount();
 
+		// Whenever a varbit is changed, record it and pass the info off to be submitted.
 		int index = varbitChanged.getIndex();
 		int[] varps = client.getVarps();
 
@@ -84,13 +91,16 @@ public class CrowdsourcingVarbits
 			int oldValue = client.getVarbitValue(oldVarps, i);
 			int newValue = client.getVarbitValue(varps, i);
 
-			if (oldValue != newValue)
+			// If the varbit is being changed on an initializing tick (when logging in),
+			// don't push out varbit changes. There are too many, and are generally uninteresting.
+			if (oldValue != newValue && tick != initializingTick)
 			{
-				// We should probably ignore the name, since there aren't many, and they can easily be found
-				// in post analysis.
-				WorldPoint playerLocation = client.getLocalPlayer().getWorldLocation();
-				VarbitData varbitData = new VarbitData(i, oldValue, newValue, tick, playerLocation);
-				crowdsourcingManager.storeEvent(varbitData);
+					// We should probably ignore the name, since there aren't many, and they can easily be found
+					// in post analysis.
+					WorldPoint playerLocation = client.getLocalPlayer().getWorldLocation();
+					VarbitData varbitData = new VarbitData(i, oldValue, newValue, tick, playerLocation);
+					log.info(varbitData.toString());
+					// crowdsourcingManager.storeEvent(varbitData);
 			}
 		}
 
